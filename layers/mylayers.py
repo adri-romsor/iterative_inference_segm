03c9caa6c784ff2aca29fe23829c1cd422d7e2af
Michał Drożdżal <michal@imagia.com>
2016-08-23 15:11:22 -0400

    add depooling layer (WWAE)

--- layers/mylayers.py
+++ layers/mylayers.py
@@ -1,5 +1,8 @@
 from lasagne.layers import MergeLayer
 from lasagne.layers.merge import autocrop, autocrop_array_shapes
+from lasagne.layers.pool import Upscale2DLayer
+import theano.tensor as T
+from lasagne.utils import as_tuple
 
 
 class ElemwiseMergeLayer(MergeLayer):
@@ -45,3 +48,41 @@ class ElemwiseMergeLayer(MergeLayer):
             else:
                 output = input
         return output
+
+class DePool2D(Upscale2DLayer):
+   ''' Code taken from:
+   https://github.com/nanopony/keras-convautoencoder/blob/master/autoencoder_layers.py
+
+    Simplar to Upscale2DLayer, yet traverse only maxpooled elements
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+    # Output shape
+        4D tensor with shape:
+        `(samples, channels, upsampled_rows, upsampled_cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, upsampled_rows, upsampled_cols, channels)` if dim_ordering='tf'.
+    # Arguments
+        size: tuple of 2 integers. The upsampling factors for rows and columns.
+    '''
+
+    def __init__(self, incoming, scale_factor, **kwargs):
+        super(Upscale2DLayer, self).__init__(incoming, **kwargs)
+        self.scale_factor = as_tuple(scale_factor, 2)
+        if self.scale_factor[0] < 1 or self.scale_factor[1] < 1:
+            raise ValueError('Scale factor must be >= 1, not {0}'.format(
+                self.scale_factor))    
+
+    def get_output_for(self, inputs, train=False, **kwargs):
+        a, b = self.scale_factor
+        upscaled = inputs.output(train)
+        if b > 1:
+            upscaled = T.extra_ops.repeat(upscaled, b, 3)
+        if a > 1:
+            upscaled = T.extra_ops.repeat(upscaled, a, 2)
+        f = T.grad(T.sum(inputs.output(train)),
+                wrt=inputs.input(train)) * upscaled
+        
+        return f 
