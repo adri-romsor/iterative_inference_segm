7827ca455b15ee33d88463b04c34bd7dae98c448
SimJeg <simon.jegou@gmail.com>
2016-08-31 17:05:52 -0400

    DenseNet is on! Still missing options

--- models/DenseNet.py
+++ models/DenseNet.py
@@ -3,8 +3,7 @@ import theano.tensor as T
 
 from lasagne.layers import (
     InputLayer, DropoutLayer, BatchNormLayer, ConcatLayer, NonlinearityLayer, Conv2DLayer, Pool2DLayer, Deconv2DLayer,
-    Upscale2DLayer, ReshapeLayer, DimshuffleLayer, get_output)
-from layers.mylayers import CroppingLayer
+    Upscale2DLayer, ReshapeLayer, DimshuffleLayer, get_output, get_all_layers, get_output_shape, get_all_param_values)
 from lasagne.nonlinearities import softmax, linear
 from lasagne.init import HeUniform
 
@@ -12,6 +11,8 @@ import model_helpers
 
 
 def buildDenseNet(nb_in_channels,
+                  n_rows=None,
+                  n_cols=None,
                   input_var=None,
                   n_classes=21,
                   n_filters_first_conv=12,
@@ -24,39 +25,75 @@ def buildDenseNet(nb_in_channels,
                   dropout_p=0.2,
                   pad_mode='same',
                   pool_mode='average',
-                  apply_dilated_after_block_index = 2,
-                  upsampling_mode='upscale',
+                  dilated_convolution_index=None,
+                  upsampling_mode='deconvolution',
+                  deconvolution_mode='keep',
+                  upsampling_block_mode='classic',
                   trainable=True):
     """
     We adapt DenseNet for segmentation
+        Reference : https://arxiv.org/pdf/1608.06993.pdf
+        GitHub : https://github.com/liuzhuang13/DenseNet
 
-    Reference : https://arxiv.org/pdf/1608.06993.pdf
-    GitHub : https://github.com/liuzhuang13/DenseNet
+    The network is organized as follow :
+
+    - Input
+    - First convolution with n_filters_first_conv filters
+    - Downsampling : n_block times Dense block + TransitionDown
+    - Bottleneck : one Dense block
+    - Upsampling : n_block times TransitionUp + Classic block or Dense block
+    - 1x1 convolution and softmax
+
+    In each Dense block, there n_conv_per_block convolution. Each convolution j is applied to an input stack of feature
+    maps [x(0) ... x(j-1)]. It produces growth_rate new feature maps x(j) which is concatenated to the stack.
+    Each convolution except the first is organized as follow : BatchNorm - ReLu - Conv - Dropout (if dropout_p > 0)
+
+    TransitionDown first performs a 1x1 convolution which preserves the number of features maps in the stack and then
+    average pooling
+
+    TransitionUp : will perform an upsampling on the stack. Depending the upsampling_mode, the number of feature maps
+    is the stack be modified
+
+    Classic block : during upsampling, we don't stack the features maps anymore, but perform usual convolution,
+    decreasing the number of features maps by growth_rate_up at each convolution. It's more U-Net like as it permits
+    symmetry
+
 
     Parameters
     ----------
     nb_in_channels : number of input channels
     input_var : tensor variable of the input
-    n_classes : number of classes
+    n_classes : number of output channels
+    n_rows and n_cols = optional, number of rows and columns of the input image
     n_filters_first_conv : number of filters to use for the first convolution of the network
-    filter_size : 3... as usual...
-    n_blocks :
-    growth_rate_down
-    growth_rate_up
-    n_conv_per_block_down
-    n_conv_per_block_up
-    dropout_p
-    pad_mode
-    pool_mode
-    upsampling_mode
-    trainable
-
-    Returns
-    -------
-
+    filter_size : filter size of the convolutions in the Dense block
+    n_blocks : number of blocks (equivalently number of pooling we apply)
+    growth_rate_down : number of new feature maps in a Dense block during Downsampling
+    growth_rate_up : number of new feature maps in a Dense block during Upsampling
+    n_conv_per_block_down : number of convolution in in a Dense block during Downsampling
+    n_conv_per_block_up : number of convolution in in a Dense block during Upsampling
+    dropout_p : dropout rate
+    pad_mode : implemented : 'same'
+    pool_mode : 'average' or 'max'
+    dilated_convolution_index (int) : must be < n_blocks. If != None (default), apply dilated convolutions
+    instead of pooling after this index. If = 3 and n_blocks = 5, there will be pool1, pool2 and pool3.
+    upsampling_mode :
+        if 'deconvolution' performs deconvolution to upsample. See deconvolution_mode
+        if 'upscale' ... you shouldn't use that
+        To implement : bilinear upsampling and WWAE
+    deconvolution_mode :
+        if 'keep', the deconvolution will preserve the number of feature maps
+        if 'reduce', the deconvolution will output growth_rate_up feature maps
+    upsampling_block_mode :
+        if 'classic' will use Classic block in during the upsampling
+        if 'dense' will use Dense block
+    trainable : freeze parameters if False
     """
 
-    # TODO : check pad options
+    if dilated_convolution_index is None:
+        dilated_convolution_index = n_blocks
+    else:
+        assert (0 < dilated_convolution_index < n_blocks)
 
     #####################
     #    Layer utils    #
@@ -64,7 +101,7 @@ def buildDenseNet(nb_in_channels,
 
     init_scheme = HeUniform()
 
-    def BN_ReLu_Conv(inputs, growth_rate, filter_size=filter_size):
+    def BN_ReLu_Conv(input_stack, growth_rate, filter_size=filter_size):
         """
         Apply successivly BatchNormalization, ReLu nonlinearity, Convolution and Dropout
         (if dropout_p > 0) on the inputs
@@ -72,10 +109,10 @@ def buildDenseNet(nb_in_channels,
         Returns
         -------
         
-        growth_rate features maps
+        growth_rate new features maps (the concatenation with inputs will be applied next)
         """
 
-        l = BatchNormLayer(inputs) #TODO : test differents parameters for BN
+        l = BatchNormLayer(input_stack)  # TODO : test differents parameters for BN
         l = NonlinearityLayer(l)
         l = Conv2DLayer(l, growth_rate, filter_size, pad=pad_mode, W=init_scheme,
                         nonlinearity=linear, flip_filters=False)
@@ -83,7 +120,7 @@ def buildDenseNet(nb_in_channels,
             l = DropoutLayer(l, dropout_p)
         return l
 
-    def TransitionDown(inputs, n_filters, block_index):
+    def TransitionDown(input_stack, n_filters, block_index):
         """
         Apply succesivly BatchNormalization, ReLu nonlinearity, Convolution (filter size = 1),
         Dropout (if dropout_p > 0) and Pooling with a factor 2 
@@ -91,28 +128,28 @@ def buildDenseNet(nb_in_channels,
         apply DilatedConvolution instead of pooling
         """
 
-        l = BatchNormLayer(inputs)
+        l = BatchNormLayer(input_stack)
         l = NonlinearityLayer(l)
         l = Conv2DLayer(l, n_filters, 1, pad=pad_mode, W=init_scheme,
                         nonlinearity=linear, flip_filters=False)
         if dropout_p:
             l = DropoutLayer(l, dropout_p)
 
-        if pool_mode == 'average':
+        if block_index > dilated_convolution_index - 1:
+            raise ValueError('Dilated convolutions not yet implemented')
+
+        elif pool_mode == 'average':
             return Pool2DLayer(l, 2, mode='average_exc_pad')
 
         elif pool_mode == 'max':
             return Pool2DLayer(l, 2, mode='max')
 
-        elif pool_mode == 'dilated':
-            raise ValueError('Not yet implemented')
-
         else:
             raise ValueError('Unknown pool_mode value : ' + pool_mode)
 
-    def TransitionUp(layer1, layer2, n_filters, block_index, filter_size=4):
+    def TransitionUp(layer_c, layer_u, n_filters, block_index, filter_size=4):
         """
-        Performs upsampling on layer2 by a factor 2 and concatenate it with the layer1
+        Performs upsampling on layer_u by a factor 2 and concatenate it with the layer_c
         If pool_mode = 'dilated' and  block_index < apply_dilated_after_block_index, upsampling is not performed
 
         Parameters
@@ -120,19 +157,26 @@ def buildDenseNet(nb_in_channels,
         filter_size : filter_size for the deconvolution
         """
 
-        if pool_mode == 'dilated':
-            #TODO insert block index condition
-            return ConcatLayer([layer1, layer2])
+        if block_index > dilated_convolution_index - 1:
+            print('coucou')
+            return ConcatLayer([layer_c, layer_u])
 
         if upsampling_mode == 'upscale':
-            return ConcatLayer([layer1, Upscale2DLayer(layer2, 2)])
-            # TODO : to delete
+            return ConcatLayer([layer_c, Upscale2DLayer(layer_u, 2)], cropping=[None, None, 'center', 'center'])
+            # TODO : delete it
 
         elif upsampling_mode == 'deconvolution':
-            l = Deconv2DLayer(layer2, n_filters, filter_size, stride=2,
-                              crop='valid', W=init_scheme, nonlinearity=linear)
-            l = CroppingLayer(l,)  # TODO finish it
+            if deconvolution_mode == 'keep':
+                n_filters_out = n_filters
+            elif deconvolution_mode == 'reduce':
+                n_filters_out = growth_rate_up
+            else:
+                raise ValueError('Unknown deconvolution_mode value : ' + deconvolution_mode)
 
+            l = Deconv2DLayer(layer_u, n_filters_out, filter_size, stride=2,
+                              crop='valid', W=init_scheme, nonlinearity=linear)
+            l = ConcatLayer([l, layer_c], cropping=[None, None, 'center', 'center'])
+            return l
 
         elif upsampling_mode == 'bilinear':
             raise ValueError('Not yet implemented')
@@ -149,25 +193,25 @@ def buildDenseNet(nb_in_channels,
     # Downsampling path #
     #####################
 
-    inputs = InputLayer((None, nb_in_channels, None, None), input_var)
+    inputs = InputLayer((None, nb_in_channels, n_rows, n_cols), input_var)
 
-    # We perform a first convolution. All the features maps will be stored in the tensor concatenation
-    concatenation = Conv2DLayer(inputs, n_filters_first_conv, filter_size, pad=pad_mode, W=init_scheme,
-                                nonlinearity=linear, flip_filters=False)
+    # We perform a first convolution. All the features maps will be stored in the tensor stack
+    stack = Conv2DLayer(inputs, n_filters_first_conv, filter_size, pad=pad_mode, W=init_scheme,
+                        nonlinearity=linear, flip_filters=False)
 
     n_filters = n_filters_first_conv
     skip_connections = []
-    for i in range(n_blocks - 1):
+    for i in range(n_blocks):
         for j in range(n_conv_per_block_down):
-            l = BN_ReLu_Conv(concatenation, growth_rate_down)
-            concatenation = ConcatLayer([concatenation, l])
+            l = BN_ReLu_Conv(stack, growth_rate_down)
+            stack = ConcatLayer([stack, l])
             n_filters += growth_rate_down
 
-        # As concatenation will be downsample, we don't upsample it again in the upsampling path but instead
+        # As stack will be downsample, we don't upsample it again in the upsampling path but instead
         # store it as standard skip connections
 
-        skip_connections.append(concatenation)
-        concatenation = TransitionDown(concatenation, n_filters, i)
+        skip_connections.append(stack)
+        stack = TransitionDown(stack, n_filters, i)
 
     skip_connections = skip_connections[::-1]
 
@@ -178,30 +222,43 @@ def buildDenseNet(nb_in_channels,
     # We store layers we'll have to upsample in a list
     layers_to_upsample = []
     for j in range(n_conv_per_block_down):
-        l = BN_ReLu_Conv(concatenation, growth_rate_down)
+        l = BN_ReLu_Conv(stack, growth_rate_down)
         layers_to_upsample.append(l)
-        concatenation = ConcatLayer([concatenation, l])
+        stack = ConcatLayer([stack, l])
         n_filters += growth_rate_down
 
     #######################
-    #  TransitionUp path  #
+    #   Upsampling path   #
     #######################
 
-    for i in range(n_blocks - 1):
-        layers_to_upsample = ConcatLayer(layers_to_upsample)
-        concatenation = TransitionUp(skip_connections[i], layers_to_upsample, n_filters, i)
-        layers_to_upsample = []
-        for j in range(n_conv_per_block_up):
-            l = BN_ReLu_Conv(concatenation, growth_rate_up)
-            n_filters += growth_rate_up
-            layers_to_upsample.append(l)
-            concatenation = ConcatLayer([concatenation, l])
+    if upsampling_block_mode == 'classic':
+        l = ConcatLayer(layers_to_upsample)
+        for i in range(n_blocks):
+            l = TransitionUp(skip_connections[i], l, n_filters, i)
+            for j in range(n_conv_per_block_up):
+                n_filters -= growth_rate_up
+                l = BN_ReLu_Conv(l, n_filters)
+
+    elif upsampling_block_mode == 'dense':
+        for i in range(n_blocks):
+            layers_to_upsample = ConcatLayer(layers_to_upsample)
+            stack = TransitionUp(skip_connections[i], layers_to_upsample, n_filters, n_blocks - i - 1)
+            layers_to_upsample = []
+            for j in range(n_conv_per_block_up):
+                l = BN_ReLu_Conv(stack, growth_rate_up)
+                n_filters += growth_rate_up
+                layers_to_upsample.append(l)
+                stack = ConcatLayer([stack, l])
+        l = stack
+
+    else:
+        raise ValueError('Unknown upsampling_block_mode value : ' + upsampling_block_mode)
 
     #####################
     #      Softmax      #
     #####################
 
-    l = Conv2DLayer(concatenation, n_classes, 1, nonlinearity=linear, 
+    l = Conv2DLayer(l, n_classes, 1, nonlinearity=linear,
                     W=init_scheme, pad=pad_mode, flip_filters=False)
 
     # We perform the softmax nonlinearity in 2 steps :
@@ -223,10 +280,57 @@ def buildDenseNet(nb_in_channels,
     if not trainable:
         model_helpers.freezeParameters(output_layer)
 
-    print('Number of features maps before softmax = {}'.format(n_filters))
-    print('Number of conv layers = {}'.format(1 + (n_conv_per_block_down + n_conv_per_block_up + 1) * n_blocks))
-
     return output_layer
+
+
+def summary(cf):
+    """
+    Print a summary of the network associated to the config cf
+    """
+
+    output_layer = buildDenseNet(
+        cf.nb_in_channels,
+        cf.train_crop_size[0],
+        cf.train_crop_size[1],
+        None,
+        cf.n_classes,
+        cf.n_filters_first_conv,
+        cf.filter_size,
+        cf.n_blocks,
+        cf.growth_rate_down,
+        cf.growth_rate_up,
+        cf.n_conv_per_block_down,
+        cf.n_conv_per_block_up,
+        cf.dropout_p,
+        cf.pad_mode,
+        cf.pool_mode,
+        cf.dilated_convolution_index,
+        cf.upsampling_mode,
+        cf.deconvolution_mode,
+        cf.upsampling_block_mode,
+        cf.trainable)
+
+    layer_list = get_all_layers(output_layer)
+
+    for layer_type in [DropoutLayer, NonlinearityLayer, BatchNormLayer, ReshapeLayer, DimshuffleLayer]:
+        layer_list = filter(lambda x: not isinstance(x, layer_type), layer_list)
+    output_shape_list = map(get_output_shape, layer_list)
+
+    layer_name = lambda s: str(s).split('.')[3].split(' ')[0]
+
+    print('-' * 75)
+    print '\n   Layer       Output shape           W shape \n'
+
+    for i, (layer, output_shape) in enumerate(zip(layer_list, output_shape_list)):
+        if isinstance(layer, Conv2DLayer):
+            input_shape = layer.W.get_value().shape
+        else:
+            input_shape = ''
+        print '{}. {}  {}  {}'.format(i, layer_name(layer), output_shape, input_shape)
+
+    print '\nNumber of conv layers : {}'.format(len(filter(lambda x: isinstance(x, Conv2DLayer), layer_list)))
+    print 'Number of parameters : {}'.format(np.sum(map(np.size, get_all_param_values(output_layer))))
+    print('-' * 75)
 
 
 if __name__ == '__main__':
@@ -240,7 +344,7 @@ if __name__ == '__main__':
                                  n_classes=21,
                                  n_filters_first_conv=12,
                                  filter_size=3,
-                                 n_blocks=5,
+                                 n_blocks=3,
                                  growth_rate_down=12,
                                  growth_rate_up=12,
                                  n_conv_per_block_down=3,
@@ -248,14 +352,17 @@ if __name__ == '__main__':
                                  dropout_p=0.2,
                                  pad_mode='same',
                                  pool_mode='average',
-                                 apply_dilated_after_block_index = 0,
-                                 upsampling_mode='upscale',
+                                 dilated_convolution_index=None,
+                                 upsampling_mode='deconvolution',
+                                 deconvolution_mode='reduce',
+                                 upsampling_block_mode='dense',
                                  trainable=True)
 
     output_tensor = get_output(output_layer)
-    f = function([x], output_tensor)
 
-    x_arr = np.ones((10, 3, 32, 32), dtype='float32')
-    y_arr = f(x_arr)
-    print y_arr
-    print y_arr.shape
+    # f = function([x], output_tensor)
+    #
+    # x_arr = np.ones((10, 3, 100, 100), dtype='float32')
+    # y_arr = f(x_arr)
+    # print y_arr
+    # print y_arr.shape
---
+++ train_densenet.py
@@ -0,0 +1,286 @@
+import os
+import argparse
+import time
+from getpass import getuser
+from distutils.dir_util import copy_tree
+
+import numpy as np
+import theano
+import theano.tensor as T
+from theano import config
+import lasagne
+from lasagne.regularization import regularize_network_params
+
+from data_loader import load_data
+from models.DenseNet import buildDenseNet, summary
+from metrics import jaccard, accuracy, crossentropy
+
+_FLOATX = config.floatX
+if getuser() == 'romerosa':
+    LOCAL_SAVEPATH = '/Tmp/romerosa/itinf/models/'
+    FINAL_SAVEPATH = '/data/lisatmp4/romerosa/itinf/models/'
+elif getuser() == 'jegousim':
+    LOCAL_SAVEPATH = '/data/lisatmp4/jegousim/iterative_inference/'
+    FINAL_SAVEPATH = '/data/lisatmp4/jegousim/iterative_inference/'
+elif getuser() == 'michal':
+    LOCAL_SAVEPATH = '/home/michal/Experiments/iter_inf/'
+else:
+    raise ValueError('Unknown user : {}'.format(getuser()))
+
+
+def train(cf):
+    # Prepare save directories
+    savepath = os.path.join(LOCAL_SAVEPATH, cf.dataset, cf.exp_name)  # local path
+    final_savepath = os.path.join(LOCAL_SAVEPATH, cf.dataset, cf.exp_name)  # remote path
+
+    for path in set([savepath, final_savepath]):  # in case savepath == final_savepath
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            print('\033[93m The following folder already exists {}. '
+                  'It will be overwritten in a few seconds...\033[0m'.format(path))
+
+    # Save and print  configuration
+    print('Saving directory : ' + savepath)
+    print('-'*75)
+    print('Config\n')
+    with open(os.path.join(savepath, "config.txt"), "w") as f:
+        for key, value in cf.__dict__.items():
+            if not key.startswith('__') & key.endswith('__'):
+                f.write('{} = {}\n'.format(key, value))
+                print('{} = {}'.format(key, value))
+    print('-'*75)
+
+    # Define symbolic variables
+    input_var = T.tensor4('input_var')
+    target_var = T.ivector('target_var')
+
+    # Build dataset iterator
+    print('Loading data')
+    train_iter, val_iter, test_iter = load_data(cf.dataset, train_crop_size=cf.train_crop_size, one_hot=False)
+
+    n_batches_train = train_iter.get_n_batches()
+    n_batches_val = val_iter.get_n_batches()
+    n_batches_test = test_iter.get_n_batches()
+    n_classes = cf.n_classes = train_iter.get_n_classes()
+    void_labels = train_iter.get_void_labels()
+
+    # Build model
+
+    print('Building model and training functions')
+    convmodel = buildDenseNet(
+        cf.nb_in_channels,
+        None, # n_rows
+        None, # n_cols
+        input_var,
+        n_classes,
+        cf.n_filters_first_conv,
+        cf.filter_size,
+        cf.n_blocks,
+        cf.growth_rate_down,
+        cf.growth_rate_up,
+        cf.n_conv_per_block_down,
+        cf.n_conv_per_block_up,
+        cf.dropout_p,
+        cf.pad_mode,
+        cf.pool_mode,
+        cf.dilated_convolution_index,
+        cf.upsampling_mode,
+        cf.deconvolution_mode,
+        cf.upsampling_block_mode,
+        cf.trainable)
+
+    summary(cf)
+
+    # Compile training functions
+
+    prediction = lasagne.layers.get_output(convmodel)
+    loss = cf.loss_function(prediction, target_var, void_labels)
+
+    weightsl2 = regularize_network_params(convmodel, lasagne.regularization.l2)
+    loss += cf.weight_decay * weightsl2
+
+    params = lasagne.layers.get_all_params(convmodel, trainable=True)
+    updates = cf.optimizer(loss, params, learning_rate=cf.learning_rate)
+
+    start_time_compilation = time.time()
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+
+    print('-'*75)
+    print('Compilation')
+    print('Train compilation took {:.3f} seconds'.format(time.time() - start_time_compilation))
+
+    # Compile test functions
+    test_prediction = lasagne.layers.get_output(convmodel, deterministic=True)
+    test_loss = cf.loss_function(test_prediction, target_var, void_labels)
+    test_acc = accuracy(test_prediction, target_var, void_labels)
+    test_jacc = jaccard(test_prediction, target_var, n_classes)
+
+    start_time_compilation = time.time()
+    val_fn = theano.function([input_var, target_var], [test_loss, test_acc, test_jacc])
+    print('Validation compilation took {:.3f} seconds'.format(time.time() - start_time_compilation))
+
+    # Parameters setup
+    err_train = []
+    err_valid = []
+    acc_valid = []
+    jacc_valid = []
+    patience = 0
+
+    # Training main loop
+    print('-' * 30)
+    print('Start training')
+    print('-' * 30)
+
+    for epoch in range(cf.num_epochs):
+        # Single epoch training and validation
+        start_time = time.time()
+        cost_train_tot = 0
+
+        # Train
+        for i in range(n_batches_train):
+            # Get minibatch
+            X_train_batch, L_train_batch = train_iter.next()
+            L_train_batch = np.reshape(L_train_batch, np.prod(L_train_batch.shape))
+
+            # Training step
+            cost_train = train_fn(X_train_batch, L_train_batch)
+            out_str = "cost %f" % (cost_train)
+            cost_train_tot += cost_train
+
+        err_train += [cost_train_tot / n_batches_train]
+
+        # Validation
+        cost_val_tot = 0
+        acc_val_tot = 0
+        jacc_val_tot = np.zeros((2, n_classes))
+        for i in range(n_batches_val):
+            # Get minibatch
+            X_val_batch, L_val_batch = val_iter.next()
+            L_val_batch = np.reshape(L_val_batch,
+                                     np.prod(L_val_batch.shape))
+
+            # Validation step
+            cost_val, acc_val, jacc_val = val_fn(X_val_batch, L_val_batch)
+
+            acc_val_tot += acc_val
+            cost_val_tot += cost_val
+            jacc_val_tot += jacc_val
+
+        err_valid += [cost_val_tot / n_batches_val]
+        acc_valid += [acc_val_tot / n_batches_val]
+        jacc_valid += [np.mean(jacc_val_tot[0, :] /
+                               jacc_val_tot[1, :])]
+
+        out_str = "EPOCH %i: Avg epoch training cost train %f, cost val %f" + \
+                  ", acc val %f, jacc val %f took %f s"
+        out_str = out_str % (epoch, err_train[epoch],
+                             err_valid[epoch],
+                             acc_valid[epoch],
+                             jacc_valid[epoch],
+                             time.time() - start_time)
+        print out_str
+
+        with open(savepath + "output.log", "a") as f:
+            f.write(out_str + "\n")
+
+        # Early stopping and saving stuff
+        if epoch == 0:
+            best_jacc_val = jacc_valid[epoch]
+        elif epoch > 1 and jacc_valid[epoch] > best_jacc_val:
+            best_jacc_val = jacc_valid[epoch]
+            patience = 0
+            np.savez(os.path.join(savepath, 'model.npz'),
+                     *lasagne.layers.get_all_param_values(convmodel))
+            np.savez(os.path.join(savepath, 'errors.npz'),
+                     err_valid, err_train, acc_valid,
+                     jacc_valid)
+        else:
+            patience += 1
+
+        # Finish training if patience has expired or max nber of epochs reached
+
+        if patience == cf.max_patience or epoch == cf.num_epochs - 1:
+            # Load best model weights
+            with np.load(os.path.join(savepath, 'model.npz')) as f:
+                param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+            nlayers = len(lasagne.layers.get_all_params(convmodel))
+            lasagne.layers.set_all_param_values(convmodel, param_values[:nlayers])
+
+            # Test
+            print('Training ends\nTest')
+            cost_test_tot = 0
+            acc_test_tot = 0
+            jacc_num_test_tot = np.zeros((1, n_classes))
+            jacc_denom_test_tot = np.zeros((1, n_classes))
+            for i in range(n_batches_test):
+                # Get minibatch
+                X_test_batch, L_test_batch = test_iter.next()
+                L_test_batch = np.reshape(L_test_batch,
+                                          np.prod(L_test_batch.shape))
+
+                # Test step
+                cost_test, acc_test, jacc_test = \
+                    val_fn(X_test_batch, L_test_batch)
+                jacc_num_test, jacc_denom_test = jacc_test
+
+                acc_test_tot += acc_test
+                cost_test_tot += cost_test
+                jacc_num_test_tot += jacc_num_test
+                jacc_denom_test_tot += jacc_denom_test
+
+            err_test = cost_test_tot / n_batches_test
+            acc_test = acc_test_tot / n_batches_test
+            jacc_test = np.mean(jacc_num_test_tot / jacc_denom_test_tot)
+
+            out_str = "FINAL MODEL: err test % f, acc test %f, jacc test %f"
+            out_str = out_str % (err_test,
+                                 acc_test,
+                                 jacc_test)
+            print out_str
+
+            # Copy files to final_savepath
+            if savepath != final_savepath:
+                print('Copying model and other training files to {}'.format(final_savepath))
+                copy_tree(savepath, final_savepath)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DenseNet training')
+    parser.add_argument('-e', '--exp_name',
+                        type=str,
+                        default=None,
+                        help='Name of the experiment')
+    cf = parser.parse_args()
+
+    # Training
+    cf.dataset = 'camvid'
+    cf.learning_rate = 0.0001
+    cf.weight_decay = 0.
+    cf.num_epochs = 500
+    cf.max_patience = 100
+    cf.loss_function = crossentropy
+    cf.optimizer = lasagne.updates.rmsprop
+
+    cf.nb_in_channels = 3  # TODO : souldn't be here!
+    cf.train_crop_size = (224, 224)
+
+    # Architecture
+
+    cf.n_filters_first_conv = 20
+    cf.filter_size = 3
+    cf.n_blocks = 5
+    cf.growth_rate_down = 12
+    cf.growth_rate_up = 12
+    cf.n_conv_per_block_down = 5
+    cf.n_conv_per_block_up = 5
+    cf.dropout_p = 0.2
+    cf.pad_mode = 'same'
+    cf.pool_mode = 'average'
+    cf.dilated_convolution_index = None
+    cf.upsampling_mode = 'deconvolution'
+    cf.deconvolution_mode = 'keep'
+    cf.upsampling_block_mode = 'classic'
+    cf.trainable = True
+
+    train(cf)
